8588e114618609a034f096d744e88478f8f80dda
tpkowastaken <protitom@gmail.com>
2026-01-30 19:56:21 +0100

    improved the accesibility of next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  cacheComponents: true,
-};
-
-export default nextConfig;
+module.exports = {
+  allowedDevOrigins: [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://127.0.0.1",
+    "http://localhost",
+    // Supabase local development origins
+    "http://127.0.0.1:54321",
+    "http://localhost:54321",
+  ],
+}
