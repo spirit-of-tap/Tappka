9feee26b1f9f1e8f139cfc60922bf5e42f00dbd2
tpkowastaken <protitom@gmail.com>
2026-01-31 18:24:35 +0100

    allowed dev origins better

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
-module.exports = {
+import type { NextConfig } from "next";
+const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "127.0.0.1",
     "localhost",
   ],
-}
+};
+export default nextConfig;
