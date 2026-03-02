1a7d4f22a6ba972197324a307e8ccf2936b8cd8c
Ondřej Kulhavý <okulhav@gmail.com>
2026-03-02 13:58:46 +0100

    feat: migrate PostHog init to instrumentation-client.ts

---
+++ instrumentation-client.ts
@@ -0,0 +1,8 @@
+import posthog from "posthog-js";
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  defaults: "2026-01-30",
+  capture_pageview: false, // Handled manually in posthog-pageview.tsx
+  capture_pageleave: true,
+});
